0d657720417ed3965a9c59c10cef64da0154c09d
ofirgady <ofirgady@gmail.com>
2022-03-20 10:32:11 +0200

    End of section 10

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule,
+    NgxSpinnerModule
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule,
+    NgxSpinnerModule
+  ]
+})
+export class SharedModule { }
