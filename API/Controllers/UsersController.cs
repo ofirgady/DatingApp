ecf215d977b0974452328e2290ab335635fbe694
ofirgady <OFirgady@gmail.com>
2022-03-27 13:09:13 +0300

    End of section 12.6

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -28,9 +29,12 @@ using Microsoft.AspNetCore.Mvc;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage,
+             users.PageSize, users.TotalCount, users.TotalPages);
 
             return Ok(users);
         }
--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -3,7 +3,9 @@ namespace API.Helpers
     public class UserParams
     {
         private const int MaxPageSize = 50;
+
         public int PageNumber { get; set; } = 1;
+        
         private int _pageSize = 10;
 
         public int PageSize
